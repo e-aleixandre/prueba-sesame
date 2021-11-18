5aafbd415fd523e18d0abe3b379bc7ddab62f6e1
E.Aleixandre <ealeixandredev@gmail.com>
2021-11-18 22:46:57 +0100

    Removes unused imports on UserController.php

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -5,16 +5,9 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class UserController extends AbstractController
 {
