891c9ea857790513edddc8167bd947c584e2f771
E.Aleixandre <ealeixandredev@gmail.com>
2021-11-19 06:52:22 +0100

    Adds data fixtures, symfony validator and UserFactory, and improves user creation

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ cd prueba-sesame
 docker-compose up -d --build
 docker exec -it php74-container composer install
 docker exec -it php74-container php bin/console doctrine:database:create
+docker exec -it php74-container php bin/console doctrine:migrations:migrate
+# Extra: Seed the database
+docker exec -it php74-container php bin/console doctrine:fixtures:load
 ```
--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -5,9 +5,13 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserController extends AbstractController
 {
@@ -15,8 +19,10 @@ class UserController extends AbstractController
      * Get all users
      *
      * @Route("/users", name="user_index", methods={ "GET" })
+     * @param UserRepository $userRepository
+     * @return JsonResponse
      */
-    public function index(UserRepository $userRepository) : Response {
+    public function index(UserRepository $userRepository): JsonResponse {
         $users = $userRepository->findAll();
 
         $usersArray = [];
@@ -32,8 +38,12 @@ class UserController extends AbstractController
      * Get a single user
      *
      * @Route("/users/{id}", name="user_show", methods={ "GET" })
+     * @param $id
+     * @param UserRepository $userRepository
+     * @return JsonResponse
      */
-    public function show($id, UserRepository $userRepository) {
+    public function show($id, UserRepository $userRepository): JsonResponse
+    {
         $user = $userRepository->find($id);
 
         if (!$user)
@@ -45,21 +55,66 @@ class UserController extends AbstractController
     /**
      *
      * @Route("/users", name="user_create", methods={"POST"})
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @param Request $request
+     * @param ValidatorInterface $validator
+     * @param EntityManagerInterface $entityManager
+     *
      */
-    public function create(EntityManagerInterface $entityManager) {
+    public function create(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager)
+    {
         $user = new User();
-        $user->setEmail("test@test.com")
-            ->setName("John Doe");
+
+        $constraints = new Assert\Collection([
+            'name' => [
+                new Assert\NotBlank()
+            ],
+            'email' => [
+                new Assert\NotBlank(),
+                new Assert\Email()
+            ]
+        ]);
+
+        $violations = $validator->validate($request->request->all(), $constraints);
+
+        if ($violations->count())
+        {
+            $errors = [];
+
+            foreach ($violations as $violation)
+                $errors[] = $violation->getMessage();
+
+            // TODO: Add the field name on each error
+            return $this->json($errors, 400);
+        }
+
+        $user->setName($request->get('name'))
+            ->setEmail($request->get('email'));
 
         $entityManager->persist($user);
         $entityManager->flush();
 
-        return $this->json([
-            "ok" => true,
-            "data" => [
-                "user" => $user
-            ]
-        ]);
+        return $this->json($user->getAsArray(), 201);
+    }
+
+    /**
+     * Deletes a user
+     *
+     * @Route("/users/{id}", name="user_delete", methods={ "DELETE" })
+     * @param $id
+     * @param UserRepository $userRepository
+     * @param EntityManagerInterface $entityManager
+     * @return JsonResponse
+     */
+    public function delete($id, UserRepository $userRepository, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $user = $userRepository->find($id);
+
+        if (!$user)
+            throw $this->createNotFoundException();
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return $this->json($user->getAsArray());
     }
 }
